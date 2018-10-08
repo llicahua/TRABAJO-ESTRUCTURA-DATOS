8c7a47984df4519111e46ca66301303d26539c65
llicahua <escorpio_malr@hotmail.com>
2018-10-08 00:04:24 -0500

    First Commit

---
+++ ListaEnlazada1/src/listaenlazada1/ListarLinealEnlazada.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listaenlazada1;
+
+import java.util.ArrayList;
+//import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author Administrador
+ */
+public class ListarLinealEnlazada {
+
+    public int start;
+
+    private int nCantidadElementos = 0;
+
+    ArrayList<Nodo> ListaLinealEnlazada;
+
+    public void AumentarCantidadElementos() {
+        this.nCantidadElementos++;
+    }
+    
+    
+    
+    public static ArrayList<Nodo> DevuelveListaEnlazada(
+            int nCantidadElementosIniciales,
+            int nValorInicialDato,
+            int nValorFinalDato,
+            int nValorInicialEnlace,
+            int nValorFinalEnlace) {
+        ArrayList<Nodo> oLista = new ArrayList<Nodo>();
+
+        //this.nCantidadElementos = nCantidadElementosIniciales;
+        
+        int enlace=0;
+        int dato = 0;
+        int aux =0;
+        for (int i = 1; i <= nCantidadElementosIniciales; i++) {
+            
+            
+            if (i == nCantidadElementosIniciales) {
+                enlace = 0;
+            } else
+            {
+                enlace=DevuleveNumeroAleatorio(nValorInicialEnlace, nValorFinalEnlace);
+            
+            }
+
+            dato = DevuleveNumeroAleatorio(nValorInicialDato, nValorFinalDato);
+            
+            Nodo onodo = new Nodo(dato, enlace);
+            
+            
+            if(i == 1)   {
+                onodo.setPosicion(1);
+                onodo.setStart(true);
+            } else {
+                onodo.setPosicion(aux);
+                onodo.setStart(false);
+            }
+            
+            aux=enlace;
+            oLista.add(onodo);
+            onodo = null;
+              
+        }
+        return oLista;
+    }
+
+    public static int DevuleveNumeroAleatorio(int p_al_vmin, int p_al_vmax) {
+        int nNumeroAleatorio = 0;
+
+        nNumeroAleatorio = (int) (Math.random() * (p_al_vmax - p_al_vmin));
+
+        return nNumeroAleatorio;
+    }
+
+    public ListarLinealEnlazada(ArrayList<Nodo> oLista, int p_inicio,
+            int p_nCantidadElementos) {
+        this.ListaLinealEnlazada = oLista;
+        this.start = p_inicio;
+        this.nCantidadElementos = p_nCantidadElementos;
+    }
+    
+    public void ImprimirListaLinealEnlazadaOrdenada(ArrayList<Nodo> Lista)
+    {
+        
+        
+    }
+    public void ImprimirLlistaLinealEnlazada() {
+
+       // JOptionPane.showMessageDialog(null, Lista);
+        int ptr = this.start;
+        int i=1;
+        while (ptr != 0) {
+            int aux = LeerEnlacePuntero(ptr);
+            
+            System.out.println("( " + i +" ) ---- > " + "[ Posición : " + ptr + "" + " Dato : " + LeerDatoPuntero(ptr) + " Enlace : " + aux + " ]" );
+            ptr = aux;
+            i++;
+        }
+        System.out.println("Numero de Elementos Lista Lineal Enlazada : " + this.nCantidadElementos );
+        
+        //JOptionPane.showMessageDialog(null, Lista2);
+    }
+    
+    
+       public int DevolverDatoListaEnlazada( int n_correlativo)
+    {
+        // n_correlativo : es el orden donde se encuentra el dato.
+        int dato = 0;
+        
+        int ptr = 0;
+        ptr = start;
+        int i = 1;
+        boolean bevaluacion = true;
+        
+        while(ptr != 0 && bevaluacion == true)
+        {
+            if (i == n_correlativo) {
+                dato = LeerDatoPuntero(ptr);
+                bevaluacion = false;
+            }
+            ptr = LeerEnlacePuntero(ptr);
+            i++;
+        } 
+        
+        return dato;
+    }
+
+    public int LeerDatoPuntero(int ptr) {
+        int dato = 0;
+        for (Nodo onodo : ListaLinealEnlazada) {
+
+            if (onodo.getPosicion() == ptr) {
+                dato = onodo.getDato();
+            }
+        }
+        return dato;
+    }
+
+    public int LeerEnlacePuntero(int ptr) {
+        int enlace = 0;
+        for (Nodo onodo : ListaLinealEnlazada) {
+
+            if (onodo.getPosicion() == ptr) {
+                enlace = onodo.getEnlace();
+            }
+        }
+        return enlace;
+    }
+}
